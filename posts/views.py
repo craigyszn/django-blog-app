93ad3302fea758e80df10a61903551a5d852c122
Craig Salgado <craigzachary.salgado@cit.edu>
2025-09-22 15:29:16 +0800

    feature(posts): add new files

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# sample data (instead of using a database model for now)
+posts = [
+    {"title": "First Post", "content": "Welcome to my blog!"},
+    {"title": "Second Post", "content": "Django makes development fun."},
+]
+
+def home(request):
+    return render(request, "posts/home.html", {"posts": posts})
+
